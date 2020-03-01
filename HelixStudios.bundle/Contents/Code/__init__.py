f8122c057f6d4afd2eac817593ed1cbb522f9628
acvigue <acvigue@me.com>
2020-02-29 20:14:57 -0500

    fix capitalization

--- HelixStudios.bundle/Contents/Code/__init__.py
+++ HelixStudios.bundle/Contents/Code/__init__.py
@@ -362,7 +362,7 @@ class HelixStudios(Agent.Movies):
 		# Try to get and process the video cast
 		metadata.roles.clear()
 		rolethumbs = list();
-		cast_list = html.xpath('//*[@Class="scene-info"]/table/tr[1]/td/a')
+		cast_list = html.xpath('//*[@class="scene-info"]/table/tbody/tr[1]/td[1]/a')
 		for cast_obj in cast_list:
 				model_href = BASE_URL % cast_obj.get('href')
 				model_page = HTML.ElementFromURL(model_href, sleep=REQUEST_DELAY)
@@ -383,7 +383,7 @@ class HelixStudios(Agent.Movies):
 				rolethumbs.append(cropped_headshot)
 
 		index = 0
-		htmlcast = html.xpath('//*[@Class="scene-info"]/table/tr[1]/td/a/text()')
+		htmlcast = html.xpath('//*[@class="scene-info"]/table/tbody/tr[1]/td[1]/a/text()')
 		for cast in htmlcast:
 			cname = cast.strip()
 			if (len(cname) > 0):
